7a04696b9a823c4025abe07c2d016b21647e0bb0
Jackson Bush <jfbush15@gmail.com>
2017-04-18 21:15:00 -0400

    trying

--- wedding_website/wedding_website/wsgi.py
+++ wedding_website/wedding_website/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedding_website.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedding_website.settings")
 
 
 from django.core.wsgi import get_wsgi_application
